be83d588858ed082989e95d45ac4e2a46b1d9a1e
Ben Scott <ben.scott@readify.net>
2015-01-27 14:45:34 +1000

    get basic copy build pipeline working

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,48 @@
+'use strict';
+
 var gulp = require('gulp');
-var plug = require('gulp-load-plugins')();
+var config = require('./gulp-config.json');
+var notify = require('gulp-notify');
+var filter = require('gulp-filter');
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var path = require('path');
 
+var gutil = plugins.loadUtils([
+	'colors', 'log'
+]);
 
-//gulp.task('help', plug.taskListing);
-gulp.task(
-	'rev-and-inject', 
-	['js', 'vendorjs', 'css', 'vendorcss'],
-	function(){
+var log = gutil.log;
+var colors = gutil.colors;
 
-	});
+// Revision and inject into index.html, then write it to the dist folder
+gulp.task('rev-and-inject', function(){
+	var index = path.join(config.paths.client, 'index.html');
+	var indexFilter = filter(['index.html']);
 
-gulp.task('build', ['rev-and-inject', 'images', 'fonts'], function() {
-	console.log('Building the optimised app');
-
-	// return gulp
-	// 	.src('')
-	// 	.pipe(plug.notify({
-	// 		onLast: true,
-	// 		message: 'Deployed code!'
-	// 	}));
+	gulp
+		// set source (/src/client/)
+		.src([].concat(index))
+		// filter to index.html
+		//.pipe(indexFilter)
+		// write to dest (/src/client-build/)
+		.pipe(gulp.dest(config.paths.dist));
+		// remove filter
+		//.pipe(indexFilter.restore());
 });
 
+// Just copy files to production for now
+gulp.task('build', ['rev-and-inject'], function() {
+	log('Building the optimised app');
+
+	// notify
+	return gulp.src('').pipe(notify({
+		onLast: true,
+		message: 'Deployed code!'
+	}));
+});
+
+gulp.task('clean', function() {
+	log('Cleaning: ' + colors.blue(config.paths.dist));
+	del([].concat(config.paths.dist));
+});
